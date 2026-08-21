03fda0cad0fec05708bbbbdf67d6332a98742363
av-dev2 <emgani@aakvatech.com>
2026-08-21 23:17:22 +0300

    refactor: drop fee and enrollment helpers moved to edu_tz

--- csf_tz/custom_api.py
+++ csf_tz/custom_api.py
@@ -462,15 +462,6 @@ def delete_doc(doctype, docname):
 		frappe.msgprint(_("{0} {1} is Deleted").format("Stock Entry", doc.name))
 
 
-def on_cancel_fees(doc, method):
-	from erpnext.accounts.utils import unlink_ref_doc_from_payment_entries
-
-	unlink_ref_doc_from_payment_entries(doc)
-	from csf_tz.bank_api import cancel_invoice
-
-	cancel_invoice(doc, "before_cancel")
-
-
 def check_validate_delivery_note(doc=None, method=None, doc_name=None):
 	if not doc and doc_name:
 		doc = frappe.get_doc("Sales Invoice", doc_name)
@@ -987,79 +978,6 @@ def make_withholding_tax_gl_entries_for_purchase(doc: Any, method: Any):
 		frappe.msgprint(
 			_("Journal Entry Created for Withholding Tax <a href='{0}'>{1}</a>").format(jv_url, jv_doc.name)
 		)
-
-
-@frappe.whitelist()
-def set_fee_abbr(doc: Any = None, method: Any = None):
-	doc.company = frappe.get_value("Fee Structure", doc.fee_structure, "company")
-	send_fee_details_to_bank = frappe.get_value("Company", doc.company, "send_fee_details_to_bank") or 0
-	if not send_fee_details_to_bank:
-		return
-	doc.abbr = frappe.get_value("Company", doc.company, "abbr")
-
-
-@frappe.whitelist()
-def enroll_all_students(self):
-	"""Enrolls students or applicants.
-
-	:param self: Program Enrollment Tool
-
-	This is created to allow enqueue of students creation.
-	The default enroll process fails when there are too many enrollments to do at a go
-	"""
-	import json
-
-	self = json.loads(self)
-	self = frappe.get_doc(dict(self))
-
-	if self.get_students_from == "Student Applicant":
-		frappe.msgprint(_("Remove student applicants that are already created"))
-
-	if len(self.students) > 30:
-		frappe.enqueue("csf_tz.custom_api.enroll_students", self=self)
-		return "queued"
-	else:
-		enroll_students(self=self)
-		return len(self.students)
-
-
-@frappe.whitelist()
-def enroll_students(self):
-	"""Enrolls students or applicants.
-
-	:param self: Program Enrollment Tool
-
-	This is a copy of ERPNext function meant to allow loading from custom doctypes and frappe.enqueue
-	Used in csf_tz.custom_api.enroll_students
-	"""
-	from education.education.api import enroll_student
-
-	total = len(self.students)
-	for i, stud in enumerate(self.students):
-		frappe.publish_realtime(
-			"program_enrollment_tool",
-			dict(progress=[i + 1, total]),
-			user=frappe.session.user,
-		)
-		if stud.student:
-			prog_enrollment = frappe.new_doc("Program Enrollment")
-			prog_enrollment.student = stud.student
-			prog_enrollment.student_name = stud.student_name
-			prog_enrollment.program = self.new_program
-			prog_enrollment.academic_year = self.new_academic_year
-			prog_enrollment.academic_term = self.new_academic_term
-			prog_enrollment.student_batch_name = (
-				stud.student_batch_name if stud.student_batch_name else self.new_student_batch
-			)
-			prog_enrollment.save()
-		elif stud.student_applicant:
-			prog_enrollment = enroll_student(stud.student_applicant)
-			prog_enrollment.academic_year = self.academic_year
-			prog_enrollment.academic_term = self.academic_term
-			prog_enrollment.student_batch_name = (
-				stud.student_batch_name if stud.student_batch_name else self.new_student_batch
-			)
-			prog_enrollment.save()
 
 
 @frappe.whitelist()

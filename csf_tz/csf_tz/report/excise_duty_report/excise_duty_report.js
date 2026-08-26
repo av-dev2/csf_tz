cdcc47d3bbe096b4896085a17e0f3d376d49ec96
av-dev2 <emgani@aakvatech.com>
2026-08-26 11:53:33 +0300

    feat: add filters that load for the excise duty report

---
+++ csf_tz/csf_tz/report/excise_duty_report/excise_duty_report.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2016, Aakvatech and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Excise Duty Report"] = {
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "fieldtype": "Date",
+            "label": "From Date",
+            "reqd": 1,
+            "default": frappe.datetime.month_start()
+        },
+        {
+            "fieldname": "to_date",
+            "fieldtype": "Date",
+            "label": "To Date",
+            "reqd": 1,
+            "default": frappe.datetime.get_today()
+        }
+    ]
+}

2a958d562a9af8b89f8232552d0f8e8b3eb18b8c
av-dev2 <emgani@aakvatech.com>
2026-09-08 23:02:43 +0300

    feat: fill VFD Provider URLs and endpoints from the settings doctype

--- csf_tz/vfd_providers/doctype/vfd_provider/vfd_provider.py
+++ csf_tz/vfd_providers/doctype/vfd_provider/vfd_provider.py
@@ -1,9 +1,57 @@
 # Copyright (c) 2023, Aakvatech Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+
+# URLs and endpoints of the providers this app talks to, keyed by settings
+# doctype because the provider record can be renamed by the user.
+PROVIDER_DEFAULTS = {
+	"DIRM VFD Settings": {
+		"test_url": "https://dirmvfd.co.tz/api_demo",
+		"live_url": "https://dirmvfd.co.tz/api_live",
+		"endpoints": {
+			"get_token": "/api/v1/session/get_token",
+			"post_receipt": "/api/v1/receipts/post",
+		},
+	},
+}
 
 
 class VFDProvider(Document):
-	pass
+	def before_insert(self):
+		self.set_endpoints()
+
+	def validate(self):
+		self.set_base_url()
+
+	def set_base_url(self):
+		"""Follow the sandbox flag so users never type a URL.
+
+		Providers without known URLs keep the base_url that was entered.
+		"""
+		defaults = PROVIDER_DEFAULTS.get(self.vfd_provider_settings)
+		if not defaults:
+			return
+
+		self.base_url = defaults["test_url"] if self.sandbox else defaults["live_url"]
+
+	def set_endpoints(self):
+		"""Add the endpoints of a known provider, keeping any the user entered."""
+		defaults = PROVIDER_DEFAULTS.get(self.vfd_provider_settings)
+		if not defaults:
+			return
+
+		entered = {row.key for row in self.attributes}
+		for key, value in defaults["endpoints"].items():
+			if key not in entered:
+				self.append("attributes", {"key": key, "value": value})
+
+	def get_endpoint(self, key):
+		"""Endpoint path stored as an attribute of this provider."""
+		for row in self.attributes:
+			if row.key == key:
+				return row.value
+
+		frappe.throw(_("Endpoint {0} is not set in VFD Provider {1}").format(key, self.name))
